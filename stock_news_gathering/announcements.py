f1ea911cfa993603354410ca88edc14377866deb
darylmathison <daryl.mathison@gmail.com>
2023-12-03 20:29:22 -0500

    Fixed retrieval bug

--- stock_news_gathering/announcements.py
+++ stock_news_gathering/announcements.py
@@ -10,16 +10,20 @@ from stock_news_gathering import config
 
 def get_stocks(the_day: datetime.datetime, settings: config.Config):
     repeat = True
-    date = the_day.strftime("%Y-%m-%d")
+    size = 0
+    date = (the_day + datetime.timedelta(days=2)).strftime("%Y-%m-%d")
     uri = settings.uri.format(apikey=settings.apikey, date=date)
     while repeat:
         r = requests.get(uri).json()
-        time.sleep(0.25)
+        time.sleep(60/5)
         if 'next_url' in r:
             uri = r['next_url'] + '&apiKey=' + settings.apikey
+            repeat = True
         else:
             repeat = False
         if 'results' in r:
+            size += len(r['results'])
+            logging.info(f"size: {size}, ex_dividend_date: {r['results'][-1]['ex_dividend_date']}")
             for asset in r['results']:
                 if asset['currency'] == 'USD':
                     yield asset
@@ -29,9 +33,14 @@ def handle(event, context):
     logging.getLogger().setLevel(logging.INFO)
     settings = config.Config()
     start = datetime.datetime.now()
-    end = start + datetime.timedelta(days=30)
-    dividends = [asset for asset in get_stocks(start, settings) if
-                 dateutil.parser.parse(asset['ex_dividend_date']) <= end]
+    end = start + datetime.timedelta(weeks=1)
+    dividends = []
+    for asset in get_stocks(start, settings):
+        if dateutil.parser.parse(asset['ex_dividend_date']).date() <= end.date():
+            dividends.append(asset)
+        else:
+            break
+
     return dividends
 
 
