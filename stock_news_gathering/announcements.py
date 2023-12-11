d7dee82156a77e744e23370905864329c89d9e8a
darylmathison <daryl.mathison@gmail.com>
2023-12-10 20:04:59 -0500

    Now saving the announcements to a db for quicker lookups.

--- stock_news_gathering/announcements.py
+++ stock_news_gathering/announcements.py
@@ -6,6 +6,7 @@ import dateutil.parser
 import requests
 
 from stock_news_gathering import config
+from stock_news_gathering.db import Announcement, TableWrapper
 
 
 def get_stocks(the_day: datetime.datetime, settings: config.Config):
@@ -18,17 +19,20 @@ def get_stocks(the_day: datetime.datetime, settings: config.Config):
             r = requests.get(uri)
             r.raise_for_status()
             r = r.json()
-            time.sleep(60 / 5)
-            if 'next_url' in r:
-                uri = r['next_url'] + '&apiKey=' + settings.apikey
+            if size > 0:
+                time.sleep(60 / 5)
+            if "next_url" in r:
+                uri = r["next_url"] + "&apiKey=" + settings.apikey
                 repeat = True
             else:
                 repeat = False
-            if 'results' in r:
-                size += len(r['results'])
-                logging.info(f"size: {size}, ex_dividend_date: {r['results'][-1]['ex_dividend_date']}")
-                for asset in r['results']:
-                    if asset['currency'] == 'USD':
+            if "results" in r:
+                size += len(r["results"])
+                logging.info(
+                    f"size: {size}, ex_dividend_date: {r['results'][-1]['ex_dividend_date']}"
+                )
+                for asset in r["results"]:
+                    if asset["currency"] == "USD":
                         yield asset
         except requests.exceptions.HTTPError as err:
             logging.info(err)
@@ -39,20 +43,36 @@ def get_stocks(the_day: datetime.datetime, settings: config.Config):
             repeat = False
 
 
-def handle(event, context):
-    logging.getLogger().setLevel(logging.INFO)
+def save_announcements(event, context):
     settings = config.Config()
+    table = TableWrapper(settings.table)
     start = datetime.datetime.now()
-    end = start + datetime.timedelta(weeks=1)
-    dividends = []
-    for asset in get_stocks(start, settings):
-        if dateutil.parser.parse(asset['ex_dividend_date']).date() <= end.date():
-            dividends.append(asset)
-        else:
-            break
+    end = start + datetime.timedelta(weeks=2)
 
-    return dividends
+    last_date = table.get_last_date(start, end)
+    if last_date.date() < (end + datetime.timedelta(days=1)).date():
+        logging.info("last date stored is %s", last_date)
+
+        for asset in get_stocks(last_date, settings):
+            if dateutil.parser.parse(asset["ex_dividend_date"]).date() <= end.date():
+                table.add_announcement(Announcement(**asset))
+            else:
+                return "OK"
+    else:
+        logging.info("date exists")
+        return "OK"
 
 
-if __name__ == '__main__':
-    print(handle(None, None))
+def get_announcements(event, context):
+    settings = config.Config()
+    table = TableWrapper(settings.table)
+    start = datetime.datetime.now()
+    end = start + datetime.timedelta(weeks=2)
+
+    announcements = table.get_announcements(start, end)
+    if announcements:
+        return announcements
+
+
+if __name__ == "__main__":
+    print(get_announcements(None, None))
